ee4afa9b8bb6f791a3b2c65990ba29cbaa8f8a95
lajkogege <lajkogege@gmail.com>
2024-03-07 15:33:17 +0100

    haz feladat

--- main.js
+++ main.js
@@ -10,5 +10,7 @@ export function fuggvenyem(szam) {
   return x
 }
 
+/*irj olyan fügvényt amely a bemenétre kapott tetszőleges számot ls szöveget karakterenként megforditja és ezzel az eredménnyel tér visza*/
+
 //console.log(fuggvenyem())
 tesztesettek_fugvenyemhez();
